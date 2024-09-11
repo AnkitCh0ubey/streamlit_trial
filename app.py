790a1ec0506e0faf06b643206a63766bc74b60bb
Ankit Choubey <142090825+AnkitCh0ubey@users.noreply.github.com>
2024-09-11 17:16:41 +0530

    added duplicacy identifier

--- app.py
+++ app.py
@@ -14,13 +14,21 @@ def save_data_to_excel(name, Choice):
         data.to_excel(file_name, index=False)
     else:
         try:
-            with pd.ExcelWriter(file_name, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-                # Getting the last row in the existing sheet
-                book = load_workbook(file_name)
-                sheet = book.active
-                startrow = sheet.max_row
 
-                data.to_excel(writer, index=False, header=False, startrow=startrow)
+            existing_data = pd.read_excel(file_name)
+            duplicate = (existing_data['Name'] == name) & (existing_data['Choice'] == choice)
+
+            if duplicate.any():
+                st.warning(f"Duplicate entry found: {name}, {choice}")
+            else:
+                with pd.ExcelWriter(file_name, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+                    # Getting the last row in the existing sheet
+                    book = load_workbook(file_name)
+                    sheet = book.active
+                    startrow = sheet.max_row
+
+                    data.to_excel(writer, index=False, header=False, startrow=startrow)
+                    st.success(f"Data saved: {name}, {choice}")
         except Exception as e:
             st.error(f"Error: {e}")
 
@@ -47,6 +55,5 @@ if st.button('Load Data'):
 if submit_button:
     if name and choice:
         save_data_to_excel(name, choice)
-        st.success(f"Data saved: {name}, {choice}")
     else:
         st.error("Please enter your name.")
